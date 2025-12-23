183b301cfd5da96af896a3108496ac443e8322b3
sahmaliyevulvi <sahmaliyevulvi004@gmail.com>
2025-12-23 04:55:44 -0500

    new

--- linux_security/0x00_linux_security_basics/0-login.sh
+++ linux_security/0x00_linux_security_basics/0-login.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
-
-# Check if the script is run as root or via sudo
-if [ "$EUID" -ne 0 ]; then
-    echo "Please run this script as root or with sudo."
-    exit 1
-fi
-
-# Display the last 5 login sessions
-last -n 5
+sudo last -F -5 "$1"
